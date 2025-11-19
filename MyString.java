585c222d62fa0dca859b2cf9bba72b1e38e1a49f
omerheifetz <omer.heifetz@post.runi.ac.il>
2025-11-19 16:49:14 +0200

    HW4-OMERHEIFETZ-Q1,3

--- MyString.java
+++ MyString.java
@@ -19,12 +19,46 @@ public class MyString {
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
         // Replace the following statement with your code
-        return null;
+        if (str == null) {
+            return null;
+        }
+        String result = "";
+        char currentChar;
+        int asciiValue;
+        for (int i=0; i<str.length(); i++) {
+            currentChar = str.charAt(i);
+            asciiValue = (int) currentChar;
+            if (asciiValue>=65 && asciiValue<=90) {
+                result = result + (char) (asciiValue + 32);
+            }
+            else {
+                result = result + str.charAt(i);
+            }
+        }
+        return result;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
         // Replace the following statement with your code
+        if (str2.length()==0) {
+            return true;
+        }
+        if (str1.length()<str2.length()) {
+            return false;
+        }
+        for (int i=0; i<(str1.length()-str2.length()+1); i++) {
+            boolean subString = true;
+            for (int j=0; j<str2.length(); j++) {
+                if (str1.charAt(i + j) != str2.charAt(j)) {
+                    subString = false;
+                    break;
+                }
+            }
+            if (subString) {
+                return true;
+            }
+        }
         return false;
     }
 }
